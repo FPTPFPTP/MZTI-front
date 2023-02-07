021fb51479b458ef715e03964337cae973edfe7c
jhjeong <51750678+khunjeong@users.noreply.github.com>
2023-02-07 21:02:37 +0900

    Feat: Recoil, React-query 추가

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,18 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import '@/styles/globals.css';
+import { useState } from 'react';
+import type { AppProps } from 'next/app';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { RecoilRoot } from 'recoil';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+    // 이렇게 해야 서로 다른 사용자와 요청 사이에 데이터가 공유되지 않는다.
+    const [queryClient] = useState(() => new QueryClient());
+
+    return (
+        <QueryClientProvider client={queryClient}>
+            <RecoilRoot>
+                <Component {...pageProps} />;
+            </RecoilRoot>
+        </QueryClientProvider>
+    );
 }
