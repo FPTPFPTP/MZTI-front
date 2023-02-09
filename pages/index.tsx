d19c120c1a9d441949d99d79e33d8ff4e908c617
jhjeong <51750678+khunjeong@users.noreply.github.com>
2023-02-09 16:53:37 +0900

    [#1] 환경세팅 emotion

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,24 @@
 import type { NextPage } from 'next';
 import { Spin } from 'antd';
+import { css } from '@emotion/react';
 
 const Home: NextPage = () => {
     return (
         <div>
             <Spin size="large" />
+            <button css={BaseButtonStyle}>test</button>
         </div>
     );
 };
 
 export default Home;
+
+export const BaseButtonStyle = css`
+    height: 60px;
+    background: #2656ff;
+    color: #fafafa;
+    font-size: 1.3rem;
+    padding: 1em;
+    box-sizing: border-box;
+    white-space: nowrap;
+`;
