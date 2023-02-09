f76a818c33f68e4c3d00a84fe8cd55025cff9770
jhjeong <51750678+khunjeong@users.noreply.github.com>
2023-02-09 17:41:03 +0900

    [#1] : 전체 레이아웃 컴포넌트 적용

--- components/Layout/index.tsx
+++ components/Layout/index.tsx
@@ -1,7 +1,17 @@
 import React from 'react';
+import { css } from '@emotion/react';
 
-const Layout = () => {
-    return <></>;
+const Layout: React.FC<any> = (props) => {
+    const { children } = props;
+    return <div css={StyledLayout}>{children}</div>;
 };
 
 export default Layout;
+
+const StyledLayout = css`
+    min-width: 300px;
+    max-width: 600px;
+    height: 100vh;
+    margin: 0 auto;
+    border: 1px solid #000000;
+`;
