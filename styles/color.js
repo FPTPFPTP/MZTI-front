073f82929a98608820c1dd20726ba0abad3aa846
jhjeong <51750678+khunjeong@users.noreply.github.com>
2023-02-11 21:04:44 +0900

    Feat: color 변수 모음

---
+++ styles/color.js
@@ -0,0 +1,33 @@
+module.exports = {
+    PRIMARY: '#45D6C8',
+
+    PURPLE_ORIGIN: '#c79ad6',
+
+    // Sencondary
+    RED_ORIGIN: '#f60303', // Error
+
+    // Others
+    BLUE_ORIGIN: '#00a3ff',
+
+    GREEN_ORIGIN: '#00E033',
+
+    WHITE: '#ffffff',
+
+    BLACK: '#000000',
+
+    // Gray Light
+    GRAY_LIGHT_1: '#fafafa', // BG/Text/InputBG
+
+    // Gray Bright
+    GRAY_BRIGHT_1: '#c4c4c4', // Text/Disabled/Placeholder
+
+    // Gray Origin
+    GRAY_ORIGIN_1: '#999999',
+
+    // Gray Strong
+    GRAY_STRONG_1: '#666666',
+    // Gray Dark
+    GRAY_DARK: '#2f2f2f', // Text/Default/Icon
+
+    BOX_SHADOW: 'rgba(0, 0, 0, 0.24)', // BoxShadow Color
+};
