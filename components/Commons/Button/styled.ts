e522bf2a1a6dcea9d0299a842110e3185f5e0135
jhjeong <51750678+khunjeong@users.noreply.github.com>
2023-02-13 18:38:59 +0900

    Refactor: Button style 수정

--- components/Commons/Button/styled.ts
+++ components/Commons/Button/styled.ts
@@ -1,6 +1,9 @@
 import { css } from '@emotion/react';
+import colors from '@styles/color';
 
 const DefaultStyle = css`
+    width: 100%;
+    min-width: 180px;
     font-size: 1.5rem;
     padding: 0.5rem 2rem;
     box-sizing: border-box;
@@ -9,22 +12,37 @@ const DefaultStyle = css`
 
 export const BaseButtonStyle = css`
     ${DefaultStyle};
-    background: #45d6c8;
+    background: ${colors.PRIMARY};
     border-radius: 25px;
-    color: #fafafa;
+    color: ${colors.WHITE};
+    box-shadow: ${colors.BOX_SHADOW};
     &:hover {
         transform: scale(1.1);
-        box-shadow: 0px 0px 8px rgba(240, 119, 153, 0.24);
+        box-shadow: ${colors.BOX_SHADOW};
+    }
+    &:disabled {
+        background: ${colors.GRAY_BRIGHT_1};
+        cursor: not-allowed;
+        &:hover {
+            transform: scale(1);
+        }
     }
 `;
 
 export const TextButtonStyle = css`
     ${DefaultStyle};
     background: none;
-    color: #000000;
+    color: ${colors.BLACK};
     text-decoration: underline;
     text-underline-position: under;
     &:hover {
         transform: scale(1.1);
+    }
+    &:disabled {
+        color: ${colors.GRAY_BRIGHT_1};
+        cursor: not-allowed;
+        &:hover {
+            transform: scale(1);
+        }
     }
 `;
