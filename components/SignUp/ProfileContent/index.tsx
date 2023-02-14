9cd8ed1008a0bcc8e0f18285b99c0e1467408e7c
jhjeong <51750678+khunjeong@users.noreply.github.com>
2023-02-14 13:43:57 +0900

    Refactor: Profile Upload 기능 1차 작업

--- components/SignUp/ProfileContent/index.tsx
+++ components/SignUp/ProfileContent/index.tsx
@@ -1,17 +1,37 @@
+import React, { ChangeEvent, useRef } from 'react';
 import { Typography } from 'antd';
-import Upload from '../Upload';
-import { Container, ProfileCard, FlexCenter } from './styled';
-import React from 'react';
-import { Button } from '@components/Commons';
+import { CameraOutlined } from '@ant-design/icons';
+import { Avatar, Button } from '@components/Commons';
+import { Container, ProfileCard, AvatarWrap, UploadWrap, FlexCenter } from './styled';
+import colors from '@styles/color';
 
 const ProfileContent = () => {
+    const profileImgInputRef = useRef<any>(null);
+
+    const updateProfileImg = async ({ target: { files } }: ChangeEvent<HTMLInputElement>) => {
+        if (files) {
+            const file = files[0];
+            console.log({ file });
+        }
+    };
+
     return (
         <div css={Container}>
             <Typography.Title level={1} style={{ margin: '2rem' }}>
                 프로필 사진을 골라주세요.
             </Typography.Title>
             <div css={ProfileCard}>
-                <Upload />
+                <div css={AvatarWrap}>
+                    <form>
+                        <Avatar alt={'프로필'} size={200} />
+                        <div css={UploadWrap} onClick={() => profileImgInputRef.current.click()}>
+                            <CameraOutlined style={{ color: colors.WHITE, fontSize: '2rem' }} />
+                            <Typography.Text style={{ color: colors.WHITE }}>Upload</Typography.Text>
+                        </div>
+                        <input ref={profileImgInputRef} type="file" name="file" style={{ display: 'none' }} onChange={updateProfileImg} />
+                    </form>
+                </div>
+
                 <div css={FlexCenter}>
                     <Typography.Text keyboard>ISFP</Typography.Text>
                     <Typography.Text strong>삽살개</Typography.Text>
--- components/SignUp/ProfileContent/styled.ts
+++ components/SignUp/ProfileContent/styled.ts
@@ -16,6 +16,33 @@ export const ProfileCard = css`
     border: 2px solid ${colors.PRIMARY};
     border-radius: 25px;
     padding: 20px;
+    gap: 1rem;
+`;
+
+export const AvatarWrap = css`
+    position: relative;
+    border-radius: 50%;
+    overflow: hidden;
+    cursor: pointer;
+    input {
+        display: none;
+    }
+`;
+
+export const UploadWrap = css`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    position: absolute;
+    top: 0;
+    left: 0;
+    backgroundcolor: rgba(0, 0, 0, 0.65);
+    width: 100%;
+    height: 100%;
+    opacity: 0;
+    &:hover {
+        opacity: 1;
+    }
 `;
 
 export const FlexCenter = css`
