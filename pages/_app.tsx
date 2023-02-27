96faab694124908caded113d87337ae36952a003
khunjeong <51750678+khunjeong@users.noreply.github.com>
2023-02-27 17:09:15 +0900

    Feat: Global css, font 적용

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,10 @@
-import '@/styles/globals.css';
 import { useState } from 'react';
 import type { AppProps } from 'next/app';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { RecoilRoot } from 'recoil';
+import { Global } from '@emotion/react';
 import Layout from '../components/Layout';
+import globalReset from '@/styles/customReset';
 
 export default function App({ Component, pageProps }: AppProps) {
     // 이렇게 해야 서로 다른 사용자와 요청 사이에 데이터가 공유되지 않는다.
@@ -12,6 +13,7 @@ export default function App({ Component, pageProps }: AppProps) {
     return (
         <QueryClientProvider client={queryClient}>
             <RecoilRoot>
+                <Global styles={globalReset} />
                 <Layout>
                     <Component {...pageProps} />
                 </Layout>
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -14,6 +14,9 @@ export default function Document() {
                 <link href="/icons/favicons/favicon-16x16.png" rel="icon" type="image/png" sizes="16x16" />
                 <link href="/icons/favicons/favicon-32x32.png" rel="icon" type="image/png" sizes="32x32" />
                 <link rel="apple-touch-icon" href="/images/icons/icon-192x192.png" />
+                {/* 폰트 적용 */}
+                <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard/dist/web/static/pretendard.css" />
+                <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Barlow+Condensed:wght@100;200;300;400;500;600;700;800;900&display=swap" />
                 <meta name="theme-color" content="#FF98BA" />
             </Head>
             <body>
