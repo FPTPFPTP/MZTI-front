5f1bfc2362599a96c74779c9f8ef311833998c9b
khunjeong <51750678+khunjeong@users.noreply.github.com>
2023-03-05 17:37:29 +0900

    Refactor: MbtiComponent 위치 이동

--- components/Commons/index.ts
+++ components/Commons/index.ts
@@ -3,4 +3,5 @@ export { default as Button } from './Button';
 export { default as Header } from './Header';
 export { default as Input } from './Input';
 export { default as Loading } from './Loading';
+export { default as MbtiContent } from './MbtiContent';
 export { default as ProgressLineBar } from './ProgressLineBar';
--- components/SignUp/index.ts
+++ components/SignUp/index.ts
@@ -1,5 +1,4 @@
 export { default as IntroduceContent } from './IntroduceContent';
-export { default as MbtiContent } from './MbtiContent';
 export { default as NicknameContent } from './NicknameContent';
 export { default as ProfileContent } from './ProfileContent';
 export { default as StepProgressBar } from './StepProgressBar';
