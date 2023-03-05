f45e37e4fff7dc288680c67e23cea72d771d8d23
khunjeong <51750678+khunjeong@users.noreply.github.com>
2023-03-05 15:03:27 +0900

    Remove: 안쓰는 코드 제거

--- components/SignUp/index.ts
+++ components/SignUp/index.ts
@@ -3,4 +3,3 @@ export { default as MbtiContent } from './MbtiContent';
 export { default as NicknameContent } from './NicknameContent';
 export { default as ProfileContent } from './ProfileContent';
 export { default as StepProgressBar } from './StepProgressBar';
-export { default as Upload } from './Upload';
