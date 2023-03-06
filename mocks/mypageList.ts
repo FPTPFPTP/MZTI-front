c7270cd8c7c57f64b3b2ceefa06879a9a989c76d
khunjeong <51750678+khunjeong@users.noreply.github.com>
2023-03-06 19:19:44 +0900

    [#7] Feat: faker package 적용

--- mocks/mypageList.ts
+++ mocks/mypageList.ts
@@ -1,11 +1,12 @@
 import dayjs from 'dayjs';
-
+import { faker } from '@faker-js/faker';
 import { IMypageWriteModel } from '@/types/mypageWrite';
 
 export const WriteListMockData = Array.from(Array(100).keys()).map(
     (id): IMypageWriteModel => ({
         id,
-        title: `게시글 ${id}`,
-        date: dayjs().format('YYYY-MM-DD'),
+        title: `게시글 ${faker.word.adjective()}`,
+        date: dayjs(faker.date.past()).format('YYYY-MM-DD'),
+        thumbnail: faker.image.animals(),
     }),
 );
--- types/mypageWrite.ts
+++ types/mypageWrite.ts
@@ -2,4 +2,5 @@ export interface IMypageWriteModel {
     id: number;
     title: string;
     date: string;
+    thumbnail: string;
 }
