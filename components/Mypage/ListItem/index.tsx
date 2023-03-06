c3f0ef789cf60f21300872b3e44ea05bf36214be
khunjeong <51750678+khunjeong@users.noreply.github.com>
2023-03-06 21:50:25 +0900

    [#7] ListItem DOM에 대한 최적화

--- components/Mypage/ListItem/index.tsx
+++ components/Mypage/ListItem/index.tsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import { useRef, useState } from 'react';
 import Link from 'next/link';
 import { Typography } from 'antd';
 import { Avatar } from '@components/Commons';
+import { useObserver } from '@/hooks/useObserver';
 import colors from '@styles/color';
 import { ListItemWrapCss } from './styled';
 
@@ -15,15 +16,31 @@ interface IListItemProps {
 const ListItem = (props: IListItemProps) => {
     const { id, title, date, thumbnail } = props;
 
+    const target = useRef(null); // 대상 ref
+    const [visible, setVisible] = useState(false); // DOM을 렌더할 조건
+
+    // isIntersecting의 경우에 DOM을 마운트 한다.
+    const onIntersect = ([entry]: any) => (entry.isIntersecting ? setVisible(true) : setVisible(false));
+
+    useObserver({
+        target,
+        onIntersect,
+        threshold: 0.1, // 화면 양끝에서 10%만 보여져도 onIntersect를 실행한다.
+    });
+
     return (
-        <Link href={`/post/${id}`} css={ListItemWrapCss}>
-            <Avatar className={'thumbnail'} src={thumbnail} alt={'게시글 이미지'} size={40} />
-            <Typography.Text className="title" style={{ color: colors.GRAY_ORIGIN_1 }}>
-                {title}
-            </Typography.Text>
-            <Typography.Text className="date" style={{ color: colors.GRAY_ORIGIN_1 }}>
-                {date}
-            </Typography.Text>
+        <Link href={`/post/${id}`} css={ListItemWrapCss} ref={target}>
+            {visible && (
+                <>
+                    <Avatar className={'thumbnail'} src={thumbnail} alt={'게시글 이미지'} size={40} />
+                    <Typography.Text className="title" style={{ color: colors.GRAY_ORIGIN_1 }}>
+                        {title}
+                    </Typography.Text>
+                    <Typography.Text className="date" style={{ color: colors.GRAY_ORIGIN_1 }}>
+                        {date}
+                    </Typography.Text>
+                </>
+            )}
         </Link>
     );
 };
