2dcbed70c8d89261e157acfa3cf5e85062a4ac62
leehyunju <hyunmail521@gmail.com>
2023-03-07 17:44:53 +0900

    add : 마이페이지 수정 patch 추가

--- utils/apis/user.ts
+++ utils/apis/user.ts
@@ -9,3 +9,27 @@ export const getMyPage = async () => {
     const res = await Axios.get('/user');
     return res.data.data;
 };
+
+// 프로필 사진 수정
+export const patchProfile = async () => {
+    const res = await Axios.patch('/user/profile');
+    return res.data.data;
+};
+
+// 닉네임 수정
+export const patchNickName = async () => {
+    const res = await Axios.patch('/user/nickname');
+    return res.data.data;
+};
+
+// mbti 수정
+export const patchMbti = async () => {
+    const res = await Axios.patch('/user/mbti');
+    return res.data.data;
+};
+
+// 한 줄 소개 수정
+export const patchIntro = async () => {
+    const res = await Axios.patch('/user/intro');
+    return res.data.data;
+};
