d147a40dca1c0ac8c220bad2f25476d65a752e57
leehyunju <hyunmail521@gmail.com>
2023-03-11 15:36:01 +0900

    feat : 비회원일 때의 마이페이지

--- pages/mypage/index.tsx
+++ pages/mypage/index.tsx
@@ -7,12 +7,15 @@ import { MypageWrap } from './styled';
 import EditSvg from '@assets/icons/edit.svg';
 import Link from 'next/link';
 import { getMyPage } from '@/utils/apis/user';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useRecoilState } from 'recoil';
 import { myPageInfo } from '@/recoil/atom/user';
+import NotUser from '@assets/icons/notUser.svg';
+import NotUserMbti from '@assets/icons/notUser_mbti.svg';
 
 const mypage = () => {
     const [myInfo, setMyInfo] = useRecoilState(myPageInfo);
+    const [isLogin, setIsLogin] = useState<boolean>(false);
 
     useEffect(() => {
         const MyInfoData = getMyPage().then((res) => {
@@ -32,8 +35,32 @@ const mypage = () => {
                 }
             />
             <div css={MypageWrap}>
-                <Profile mbti={myInfo.mbti} nickname={myInfo.nickname} intro={myInfo.intro} />
-                <Write write={12} comment={73} recommend={514} />
+                {isLogin ? (
+                    <>
+                        <Profile mbti={myInfo.mbti} nickname={myInfo.nickname} intro={myInfo.intro} />
+                        <Write write={12} comment={73} recommend={514} />
+                    </>
+                ) : (
+                    <div className="notUser">
+                        <div className="notUser__flex">
+                            <div className="notUser__left">
+                                <div className="profile">
+                                    <NotUser />
+                                </div>
+                                <p>Lv.0</p>
+                            </div>
+                            <div className="notUser__right">
+                                <NotUserMbti />
+                                <h3>로그인해주세요</h3>
+                                <p>로그인을 통해 MZTI의 더 많은 기능을 경험해보세요</p>
+                            </div>
+                        </div>
+                        <Link href="/login" className="loginButton">
+                            회원가입 / 로그인
+                        </Link>
+                    </div>
+                )}
+
                 <Banner />
                 <Menu />
             </div>
