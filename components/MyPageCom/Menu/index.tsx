27d0c6f525d11d409b595443fd69a51d8a9a2f80
leehyunju <hyunmail521@gmail.com>
2023-03-11 15:17:04 +0900

    feat : mypage 공지사항, 로그아웃 탭 추가

--- components/MyPageCom/Menu/index.tsx
+++ components/MyPageCom/Menu/index.tsx
@@ -35,6 +35,26 @@ const Menu = () => {
                     </span>
                 </Link>
             </li>
+            <li>
+                <Link href="">
+                    <span>공지사항</span>
+                    <span>
+                        <svg width="9" height="15" viewBox="0 0 9 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M1 1L7.5 7.5L1 14" stroke="#A7A7A7" strokeLinecap="round" />
+                        </svg>
+                    </span>
+                </Link>
+            </li>
+            <li>
+                <Link href="">
+                    <span>로그아웃</span>
+                    <span>
+                        <svg width="9" height="15" viewBox="0 0 9 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M1 1L7.5 7.5L1 14" stroke="#A7A7A7" strokeLinecap="round" />
+                        </svg>
+                    </span>
+                </Link>
+            </li>
         </ul>
     );
 };
