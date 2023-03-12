86db613b9714b91ce2b663dd9afc28e443210d4b
leehyunju <hyunmail521@gmail.com>
2023-03-12 16:05:32 +0900

    [#15] fix : 받아오는 데이터 props로 통일

--- components/MyPageCom/Profile/index.tsx
+++ components/MyPageCom/Profile/index.tsx
@@ -1,17 +1,13 @@
 import { ProfileStyle } from './styled';
 import { IUserModel } from '@/types/user';
 import Avatar from '@/components/Commons/Avatar';
-import { useRecoilValue } from 'recoil';
-import { myPageInfo } from '@/recoil/atom/user';
 
-const Profile = ({ mbti, nickname, intro }: IUserModel) => {
-    const myProfile = useRecoilValue(myPageInfo);
-
+const Profile = ({ mbti, nickname, intro, profileImage }: IUserModel) => {
     return (
         <section css={ProfileStyle}>
             <div className="photo">
                 {/* TODO: 나중에 이미지 태그로 변경  */}
-                <Avatar size={200} alt="나의 이미지" src={myProfile.profileImage ? myProfile.profileImage : ''} />
+                <Avatar size={200} alt="나의 이미지" src={profileImage ? profileImage : ''} />
                 <span>Lv.1</span>
             </div>
             <div>
