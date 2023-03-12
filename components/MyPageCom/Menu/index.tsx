ba6039b2e874a3eeca46525b5b2077d7cd230402
leehyunju <hyunmail521@gmail.com>
2023-03-12 16:02:11 +0900

    [#15] 코드리뷰 반영 - menu 페이지

--- components/MyPageCom/Menu/index.tsx
+++ components/MyPageCom/Menu/index.tsx
@@ -1,60 +1,35 @@
 import Link from 'next/link';
 import { MenuStyle } from './styled';
 
-const Menu = () => {
+type Props = {
+    menuList: menuProps[];
+};
+type menuProps = {
+    title: string; // 메뉴명
+    url: string; // 메뉴 url
+    subDesc: string;
+};
+
+const Menu = ({ menuList }: Props) => {
     return (
         <ul css={MenuStyle}>
-            <li>
-                <Link href="">
-                    <span>내가 북마크 한 글</span>
+            {menuList.map((item: menuProps, index: number) => {
+                return (
+                    <li key={index}>
+                        <Link href={item.url}>
+                            <span>
+                                {item.title} <strong>{item.subDesc}</strong>
+                            </span>
 
-                    <span>
-                        <svg width="9" height="15" viewBox="0 0 9 15" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M1 1L7.5 7.5L1 14" stroke="#A7A7A7" strokeLinecap="round" />
-                        </svg>
-                    </span>
-                </Link>
-            </li>
-            <li>
-                <Link href="">
-                    <span>서포트 센터</span>
-                    <span>
-                        <svg width="9" height="15" viewBox="0 0 9 15" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M1 1L7.5 7.5L1 14" stroke="#A7A7A7" strokeLinecap="round" />
-                        </svg>
-                    </span>
-                </Link>
-            </li>
-            <li>
-                <Link href="">
-                    <span>계정 관리</span>
-                    <span>
-                        <svg width="9" height="15" viewBox="0 0 9 15" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M1 1L7.5 7.5L1 14" stroke="#A7A7A7" strokeLinecap="round" />
-                        </svg>
-                    </span>
-                </Link>
-            </li>
-            <li>
-                <Link href="">
-                    <span>공지사항</span>
-                    <span>
-                        <svg width="9" height="15" viewBox="0 0 9 15" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M1 1L7.5 7.5L1 14" stroke="#A7A7A7" strokeLinecap="round" />
-                        </svg>
-                    </span>
-                </Link>
-            </li>
-            <li>
-                <Link href="">
-                    <span>로그아웃</span>
-                    <span>
-                        <svg width="9" height="15" viewBox="0 0 9 15" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M1 1L7.5 7.5L1 14" stroke="#A7A7A7" strokeLinecap="round" />
-                        </svg>
-                    </span>
-                </Link>
-            </li>
+                            <span>
+                                <svg width="9" height="15" viewBox="0 0 9 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M1 1L7.5 7.5L1 14" stroke="#A7A7A7" strokeLinecap="round" />
+                                </svg>
+                            </span>
+                        </Link>
+                    </li>
+                );
+            })}
         </ul>
     );
 };
--- pages/mypage/index.tsx
+++ pages/mypage/index.tsx
@@ -12,6 +12,29 @@ import { useRecoilState } from 'recoil';
 import { myPageInfo } from '@/recoil/atom/user';
 import NotUser from '@/components/MyPageCom/NotUser';
 
+const menuList = [
+    {
+        title: '내가 북마크 한 글',
+        subDesc: '',
+        url: '',
+    },
+    {
+        title: '서포트 센터',
+        subDesc: 'FAQ, 1:1 문의, 피드백페이지',
+        url: '',
+    },
+    {
+        title: '공지사항',
+        subDesc: '업데이트 소식, 게시판 추가 등',
+        url: '',
+    },
+    {
+        title: '계정 관리',
+        subDesc: '로그아웃 등',
+        url: '',
+    },
+];
+
 const mypage = () => {
     const [myInfo, setMyInfo] = useRecoilState(myPageInfo);
     const [isLogin, setIsLogin] = useState<boolean>(false);
@@ -44,7 +67,7 @@ const mypage = () => {
                 )}
 
                 <Banner />
-                <Menu />
+                <Menu menuList={menuList} />
             </div>
         </>
     );
