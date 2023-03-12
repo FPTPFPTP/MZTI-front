e847e8997c77153f5f6a676f7467e05fd8a23a0e
정재홍 <51750678+khunjeong@users.noreply.github.com>
2023-03-12 18:10:53 +0900

    mypage writeList page 2차 PR 올립니다. (#13)
    * [#7] Feat: faker package 적용
    
    * [#7] Refactor: Image 가져오기 LazyLoadImage 적용
    
    * [#7] useObserver hook 적용
    
    * [#7] ListItem DOM에 대한 최적화
    
    * [#7] Refactor: 스크롤바 hide 적용
    
    * [#7] Refactor: 내가 작성한 댓글 리스트 수정
    
    * Squashed commit of the following:
    
    commit 5d993c528b443176c73dd7fec4279d11b107c960
    Author: FE LEE <71499150+leemember@users.noreply.github.com>
    Date:   Sun Mar 12 17:55:30 2023 +0900
    
        마이페이지 및 마이페이지 수정하기 작업완료 코드리뷰 요청드립니다. (#15)
    
        * refactore : style reset 수정, 로그인 버튼 너비 수정
    
        * fix : 이미지변경 및 추가, 마크업 수정
    
        * refactore : uuid 사용해서 atom key 유니크한 명명으로 지정
    
        * error : Duplicate atom key "signupProfileFileStates/function v1(options, buf, offset) { 해당 오류 해결
    
        * feat : atom 키값 중복 에러 해결
        타입명 변경, 토큰값 변경, 마이페이지 레이아웃 맞춤, 마크업 재정비, api 작업, SingnUp 프로필 이미지 사이즈
    
        * fix : mypage 이름 겹치는 것 있어서 components/mypagecom 으로 이름 변경
    
        * add : 마이페이지 수정 patch 추가
    
        * fix : atom key 중복되는거 방지
    
        * error fix :
    
        import { Empty, ListBox, ListItem } from '@/components/MyPageCom';
        import { useGetComments } from '@apis/mypage';
    
        이처럼 mypage 라는 키워드가 중복된다는 점에서 에러문구 떠서, mypageCom으로 수정
    
        * feat : 마이페이지 정보 불러오기
    
        * feat: 마이페이지 편집 작업 완료
    
        * feat : mypage 공지사항, 로그아웃 탭 추가
    
        * feat : 비회원일 때의 마이페이지
    
        * fix : 마이페이지 비회원일 경우 컴포넌트로 쪼개기
    
        * [#15] 코드리뷰 반영 - menu 페이지
    
        * [#15] fix : 불필요한 타입 제거
    
        * [#15] fix : 받아오는 데이터 props로 통일
    
        * [#15] fix : 전체 코드리뷰 반영완료했습니다.
    
    commit 34495c83a1ea38588f9d976f897453d34f8374b4
    Author: khunjeong <51750678+khunjeong@users.noreply.github.com>
    Date:   Wed Mar 8 20:39:22 2023 +0900
    
        Feat: IResponseBase 타입 추가
    
    * Refactor: PR 피드백 적용

---
+++ hooks/useObserver.ts
@@ -0,0 +1,31 @@
+import { useLayoutEffect, RefObject } from 'react';
+
+interface IUseObserverProps {
+    target: RefObject<HTMLElement> | null; // 감지할 대상, ref를 넘길 예정
+    onIntersect: IntersectionObserverCallback; // 감지 시 실행할 callback 함수
+    root?: Element | Document | null; // 교차할 부모 요소, 아무것도 넘기지 않으면 document가 기본이다.
+    rootMargin?: string; // root와 target이 감지하는 여백의 거리
+    threshold?: number | number[]; // 임계점. 1.0이면 root내에서 target이 100% 보여질 때 callback이 실행된다.
+}
+
+export const useObserver = ({ target, onIntersect, root = null, rootMargin = '0px', threshold = 1.0 }: IUseObserverProps) => {
+    useLayoutEffect(() => {
+        let observer: IntersectionObserver | undefined;
+        // 넘어오는 element가 있어야 observer를 생성할 수 있도록 한다.
+        if (target && target.current) {
+            // callback의 인자로 들어오는 entry는 기본적으로 순환자이기 때문에
+            // 복잡한 로직을 필요로 할때가 많다.
+            // callback을 선언하는 곳에서 로직을 짜서 통째로 넘기도록 하겠다.
+            observer = new IntersectionObserver(onIntersect, {
+                root,
+                rootMargin,
+                threshold,
+            });
+            // 실제 Element가 들어있는 current 관측을 시작한다.
+            observer.observe(target.current);
+        }
+
+        // observer를 사용하는 컴포넌트가 해제되면 observer 역시 꺼준다.
+        return () => observer && observer.disconnect();
+    }, [target, root, rootMargin, threshold]);
+};
