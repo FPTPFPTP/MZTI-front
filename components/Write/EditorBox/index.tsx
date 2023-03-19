431d4a46a3bff68f14be7fc9180883cc395aaf68
khunjeong <51750678+khunjeong@users.noreply.github.com>
2023-03-19 17:43:37 +0900

    [#14] Feat: 글쓰기 페이지 1차 작업
    - 투표 쓰기, 미리보기 적용
    - 글쓰기, 투표 작성 기능 API 적용

--- components/Write/EditorBox/index.tsx
+++ components/Write/EditorBox/index.tsx
@@ -1,12 +1,17 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { useRouter } from 'next/router';
+import { message } from 'antd';
 import { useForm } from 'react-hook-form';
 import { Editor } from '@toast-ui/react-editor';
 import { Header, Input } from '@components/Commons';
 import { Layout } from '@styles/pages/signupStyled';
 import EditSvg from '@assets/icons/edit.svg';
+import VoteSvg from '@assets/icons/vote.svg';
 import ToastEditor from '@/components/Commons/ToastEditor';
-
+import { BottomWrapper } from './styled';
+import { DefaultModeEditor as SurveyEditor, DefaultModeViewer, SurveyType } from '@khunjeong/basic-survey-template';
+import { postWrite } from '@apis/write';
+import { IPollModel } from '@/types/post';
 interface IEditorBox {
     contents: string;
 }
@@ -14,6 +19,8 @@ interface IEditorBox {
 const EditorBox = (props: IEditorBox) => {
     const { contents } = props;
     const router = useRouter();
+    const [isVote, setIsVote] = useState<boolean>(false);
+    const [surveyData, setSurveyData] = useState<SurveyType.IDefaultModeSurveyResult[]>([]);
 
     const editorRef = useRef<Editor>(null);
     const { register, watch, handleSubmit, reset, setValue } = useForm();
@@ -24,16 +31,50 @@ const EditorBox = (props: IEditorBox) => {
     };
 
     // 등록 버튼 핸들러
-    const handleRegisterButton = () => {
+    const handleRegisterButton = async () => {
         if (editorRef.current) {
-            console.log('11231231');
-
             // 입력창에 입력한 내용을 HTML 태그 형태로 취득
             console.log(editorRef.current?.getInstance().getHTML());
             // 입력창에 입력한 내용을 MarkDown 형태로 취득
             console.log(editorRef.current?.getInstance().getMarkdown());
+            try {
+                const tagList: any = [];
+                const pollList = [];
+
+                if (surveyData.length) {
+                    for (const survey of surveyData) {
+                        pollList.push({
+                            title: survey.title,
+                            startDate: survey.startDate,
+                            endDate: survey.endDate,
+                            checkCount: survey.maxChoice,
+                            items: survey.questions,
+                        });
+                    }
+                }
+                const data = await postWrite({
+                    title,
+                    categoryId: 1,
+                    content: editorRef.current?.getInstance().getHTML(),
+                    tagList,
+                    pollList,
+                });
+
+                console.log({ data });
+            } catch (error) {
+                console.log(error);
+            }
         }
     };
+
+    const onSurveyClose = () => {
+        setIsVote(false);
+        setSurveyData([]);
+    };
+
+    useEffect(() => {
+        console.log({ surveyData });
+    }, [surveyData]);
 
     useEffect(() => {
         if (editorRef.current) {
@@ -77,6 +118,25 @@ const EditorBox = (props: IEditorBox) => {
                 <button type="submit" />
             </form>
             <ToastEditor ref={editorRef} />
+            {surveyData.map((survey) => (
+                <DefaultModeViewer
+                    key={survey.id}
+                    survey={survey}
+                    onResult={() => console.log('결과 페이지 이동')}
+                    onSubmit={(result) => console.log({ result })}
+                />
+            ))}
+
+            {isVote && (
+                <div>
+                    <SurveyEditor onSubmit={(result) => setSurveyData([...surveyData, result])} onClose={onSurveyClose} submitButtonText={'첨부하기'} />
+                </div>
+            )}
+            <div css={BottomWrapper}>
+                <button onClick={() => setIsVote(true)}>
+                    <VoteSvg />
+                </button>
+            </div>
         </div>
     );
 };
