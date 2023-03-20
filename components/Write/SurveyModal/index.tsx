bd64c53b1ecdcae26cc0b2c3a38161f440d89f7e
khunjeong <51750678+khunjeong@users.noreply.github.com>
2023-03-20 16:19:27 +0900

    [#14] Refactor: 투표 이미지 업로드 기능 적용

--- components/Write/SurveyModal/index.tsx
+++ components/Write/SurveyModal/index.tsx
@@ -1,4 +1,6 @@
+import { useMemo } from 'react';
 import { Modal } from 'antd';
+import { Cookies } from 'react-cookie';
 import { DefaultModeEditor as SurveyEditor, SurveyType } from '@khunjeong/basic-survey-template';
 
 interface ISurveyModalProps {
@@ -10,9 +12,20 @@ interface ISurveyModalProps {
 const SurveyModal = (props: ISurveyModalProps) => {
     const { isModal, handleOk, handleCancel } = props;
 
+    // 업로드 옵션 설정
+    const uploadOptions: SurveyType.IUploadOptions = useMemo(() => {
+        const cookies = new Cookies();
+
+        return {
+            baseUrl: 'http://ip-set-nlp-2cce1cff97b66aa1.elb.ap-northeast-2.amazonaws.com', // proxy url
+            actionUrl: '/post/image', // 업로드 url 경로
+            authorization: cookies.get('refreshToken'), // 토큰
+        };
+    }, []);
+
     return (
         <Modal title="투표 작성하기" open={isModal} onCancel={handleCancel} footer={[]}>
-            <SurveyEditor onSubmit={handleOk} onClose={handleCancel} submitButtonText={'첨부하기'} />
+            <SurveyEditor onSubmit={handleOk} submitButtonText={'첨부하기'} uploadOptions={uploadOptions} />
         </Modal>
     );
 };
