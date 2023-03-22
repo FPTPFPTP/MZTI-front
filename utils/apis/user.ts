a9c3d314f3c4b1c0314d7f1e45c4fd483388997d
leehyunju <hyunmail521@gmail.com>
2023-03-23 05:53:05 +0900

    [#14] feat : 게시글 api

--- utils/apis/user.ts
+++ utils/apis/user.ts
@@ -5,3 +5,8 @@ export const getMyPage = async () => {
     const res = await Axios.get('/user');
     return res.data.data;
 };
+
+export const getFeedPost = async () => {
+    const res = await Axios.get('/post');
+    return res.data.data.list;
+};
