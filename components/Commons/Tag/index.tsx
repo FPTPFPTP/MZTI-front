6916700876d456744e75678040484697ce36e002
khunjeong <51750678+khunjeong@users.noreply.github.com>
2023-03-22 22:57:27 +0900

    [#14] Feat: Tag Component 추가

---
+++ components/Commons/Tag/index.tsx
@@ -0,0 +1,30 @@
+import CircleCloseSvg from '@assets/icons/circle_close.svg';
+import { TagWrapStyle } from './styled';
+
+interface ITag {
+    title: string;
+    onClick: () => void;
+    onDelete?: () => void;
+}
+
+const Tag = (props: ITag) => {
+    const { title, onClick, onDelete } = props;
+
+    const handleDelete = (e: React.MouseEvent) => {
+        e.preventDefault();
+        onDelete && onDelete();
+    };
+
+    return (
+        <li css={TagWrapStyle} onClick={onClick}>
+            {title}
+            {onDelete && (
+                <button onClick={handleDelete}>
+                    <CircleCloseSvg />
+                </button>
+            )}
+        </li>
+    );
+};
+
+export default Tag;
