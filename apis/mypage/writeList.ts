e461e2e489fae392861b4f9bd3580b856aa7d7d7
khunjeong <51750678+khunjeong@users.noreply.github.com>
2023-03-26 13:02:52 +0900

    Refactor: proxy 이슈로 mock data 불러오는 방법 수정

--- apis/mypage/writeList.ts
+++ apis/mypage/writeList.ts
@@ -1,9 +1,9 @@
-import Axios from '@utils/axios';
+import axios from 'axios';
 import { IPaginationResponse } from '@/types/global';
 import { IMypageWriteModel } from '@/types/mypageWrite';
 
 export const getWriteList = async ({ pageParam, search }: { pageParam: number; search: string }) => {
-    const res = await Axios.get<IPaginationResponse<IMypageWriteModel>>('/api/mypageList', { params: { pageParam, search } });
+    const res = await axios.get<IPaginationResponse<IMypageWriteModel>>('/api/mypageList', { params: { pageParam, search } });
 
     return res.data;
 };
