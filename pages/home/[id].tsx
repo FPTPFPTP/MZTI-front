b0b54f9315fd891710f7409a84bfbf1c6378642e
khunjeong <51750678+khunjeong@users.noreply.github.com>
2023-03-26 22:53:23 +0900

    [#14] Feat: ToastViewer 추가

--- pages/home/[id].tsx
+++ pages/home/[id].tsx
@@ -1,59 +1,65 @@
-import { GetStaticPaths, GetStaticProps, GetServerSideProps } from 'next';
+import { GetServerSideProps, NextPageContext } from 'next';
+import dynamic from 'next/dynamic';
 import { useEffect, useState } from 'react';
 import { Header } from '@components/Commons';
 import Head from 'next/head';
 import BookMarkIcon from '@assets/icons/header/HeaderBookMark.svg';
 import FillBookMarkIcon from '@assets/icons/header/HeaderBookMarkFill.svg';
-import { PostStyle, PostContent } from './styled';
 import ItemHeader from '@/components/Home/FeedItem/ItemHeader';
 import ItemFooter from '@/components/Home/FeedItem/ItemFooter';
 import FeedComents from '@/components/Home/FeedComents';
 import { useRouter } from 'next/router';
 import Axios from '@utils/axios';
 import xss from 'xss';
+import { PostStyle, PostContent } from './styled';
 import { IResponseBase } from '@/types/global';
+import { IPostModel } from '@/types/post';
+
+const ToastViewer = dynamic(() => import('@/components/Commons/ToastViewer'), {
+    ssr: false,
+});
+
+interface IPostProps {
+    data: IPostModel | null;
+    // commentData: any;
+}
 
 export const getServerSideProps: GetServerSideProps = async ({ req, params }: any) => {
     let data = null;
-    let commentData = null;
+    // let commentData = null;
     try {
         const token = req.cookies['refreshToken'];
         Axios.defaults.baseURL = 'http://ip-set-nlp-2cce1cff97b66aa1.elb.ap-northeast-2.amazonaws.com/';
         Axios.defaults.headers.common['Authorization'] = token ? `Bearer ${token}` : '';
-        const res = await Axios.get<IResponseBase<any>>(`/post/${params.id}`);
-        const commentRes = await Axios.get<IResponseBase<any>>('/post/comment', {
-            params: { postId: params.id, page: 0, view: 10 },
-        });
-        console.log('res', res);
-        console.log('commentRes', commentRes);
-        data = res.data;
-        commentData = commentRes.data.data.list;
+        const res = await Axios.get<IResponseBase<IPostModel>>(`/post/${params.id}`);
+        // const commentRes = await Axios.get<IResponseBase<any>>('/post/comment', {
+        //     params: { postId: params.id, page: 0, view: 10 },
+        // });
+        data = res.data.data;
+        // commentData = commentRes.data.data.list;
     } catch (err) {
         console.log('error', err);
     }
 
-    console.log('daad---------->', data);
-    console.log('commentData---------->', commentData);
     return {
         props: {
             data,
-            commentData,
+            // commentData,
         },
     };
 };
 
-const post = ({ data, commentData }: any) => {
-    const router = useRouter();
-    const { id } = router.query;
+const post = ({ data }: IPostProps) => {
     const [isBookMark, setIsBookMark] = useState<boolean>(false);
 
     const handleBookMark = () => [setIsBookMark((isBookMark) => !isBookMark)];
 
+    useEffect(() => {
+        console.log({ data });
+    }, [data]);
+
     return (
         <main>
-            <Head>
-                <title>MZTI | 게시판</title>
-            </Head>
             {/* 헤더 */}
             <Header
                 isPrevBtn={true}
@@ -65,29 +71,33 @@ const post = ({ data, commentData }: any) => {
                 }
             />
 
-            <div css={PostStyle}>
-                <div className="postHeaderWrap">
-                    <h3 className="postTitle">{data.data.title}</h3>
-                    <ItemHeader
-                        nickname={data.data.writer.nickname}
-                        mbti={data.data.writer.mbti}
-                        level={data.data.writer.level}
-                        profileImage={data.data.writer.profileImage}
-                        moreBtn={false}
-                        createAt={data.data.createAt}
-                    />
-                </div>
-            </div>
-
-            <article
-                dangerouslySetInnerHTML={{
-                    __html: xss(data.data.content),
-                }}
-            />
+            {data && (
+                <>
+                    <div css={PostStyle}>
+                        <div className="postHeaderWrap">
+                            <h3 className="postTitle">{data.title}</h3>
+                            <ItemHeader
+                                nickname={data.writer.nickname}
+                                mbti={data.writer.mbti}
+                                level={data.writer.level}
+                                profileImage={data.writer.profileImage}
+                                moreBtn={false}
+                                createAt={data.createAt}
+                            />
+                        </div>
+                    </div>
+                    <ToastViewer contentHtml={data.content} />
+                    {/* <article
+                        dangerouslySetInnerHTML={{
+                            __html: xss(data.content),
+                        }}
+                    /> */}
+                </>
+            )}
 
             <ItemFooter className="postFooter" isFeed={false} />
 
-            {commentData.length > 0 && (
+            {/* {commentData.length > 0 && (
                 <FeedComents
                     nickname={commentData.writer.nickname}
                     mbti={commentData.writer.mbti}
@@ -96,7 +106,7 @@ const post = ({ data, commentData }: any) => {
                     comment={commentData.comment}
                     like={commentData.like.count}
                 />
-            )}
+            )} */}
         </main>
     );
 };
