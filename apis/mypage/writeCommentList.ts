e051d8a7b9d6b351d4fe400a0744856ef8e54dbb
leehyunju <hyunmail521@gmail.com>
2023-03-28 19:59:08 +0900

    코드리뷰 반영 -> 원래대로 돌려놓았습니다~

--- apis/mypage/writeCommentList.ts
+++ apis/mypage/writeCommentList.ts
@@ -3,7 +3,7 @@ import { IPaginationResponse } from '@/types/global';
 import { IMypageWriteModel } from '@/types/mypageWrite';
 
 export const getWriteCommentList = async ({ pageParam, search }: { pageParam: number; search: string }) => {
-    const res = await Axios.get<IPaginationResponse<IMypageWriteModel>>('/post/comment/me', { params: { pageParam, search } });
+    const res = await Axios.get<IPaginationResponse<IMypageWriteModel>>('/api/mypageCommentList', { params: { pageParam, search } });
 
     return res.data;
 };
