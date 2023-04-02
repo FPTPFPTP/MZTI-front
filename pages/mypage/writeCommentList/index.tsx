ae05cdd85a886d973c81a15e6cada4c556386616
leehyunju <hyunmail521@gmail.com>
2023-04-03 02:58:29 +0900

    fix : 헤더 쪽 padding: 33px 이중으로 먹히는거 해결

--- pages/mypage/writeCommentList/index.tsx
+++ pages/mypage/writeCommentList/index.tsx
@@ -2,12 +2,10 @@ import React, { useCallback, useEffect, useRef, useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { Header, Input, Loading } from '@components/Commons';
 import { Empty, ListBox, ListItem } from '@components/MyPageCom';
-// import { useObserver } from '@/hooks/useObserver';
-// import { useGetComments } from '@apis/mypage';
-import { useGetPostCommentsMe } from '@apis/posts';
 
 import EditSvg from '@assets/icons/edit.svg';
 import { Layout } from '@styles/pages/mypageStyled';
+import { useGetPostCommentsMe } from '@/apis/post';
 
 const WriteCommentList = () => {
     const observerRef = useRef(null);
@@ -16,16 +14,6 @@ const WriteCommentList = () => {
     const { search } = watch();
 
     const { contents: commentList, hasNextPage, fetchNextPage } = useGetPostCommentsMe(search);
-
-    // // useObserver로 넘겨줄 callback, entry로 넘어오는 HTMLElement가
-    // // isIntersecting이라면 무한 스크롤을 위한 fetchNextPage가 실행될 것이다.
-    // const onIntersect = ([entry]: any) => entry.isIntersecting && fetchNextPage();
-
-    // // useObserver로 observerRef와 onIntersect를 넘겨 주자.
-    // useObserver({
-    //     target: observerRef,
-    //     onIntersect,
-    // });
 
     const handleObserver = useCallback(
         (entries: IntersectionObserverEntry[]) => {
@@ -49,29 +37,31 @@ const WriteCommentList = () => {
     }, [fetchNextPage, hasNextPage, handleObserver]);
 
     return (
-        <div css={Layout}>
+        <>
             <Header title={'내가 작성한 댓글'} rightElement={<EditSvg />} />
-            <form>
-                <Input
-                    inputStyle={'search'}
-                    placeholder={'댓글 내용 검색'}
-                    isResetBtn={!!search}
-                    handleReset={() => reset()}
-                    maxLength={8}
-                    {...register('search')}
-                />
-            </form>
-            <ListBox>
-                {commentList.length ? (
-                    commentList.map((item) => <ListItem key={item.id} id={item.id} content={item.content} createAt={item.createAt} />)
-                ) : (
-                    <Empty title="작성한 댓글이 없습니다" subTitle="첫 댓글을 남겨보러 갈까요?" buttonTitle="댓글 작성하러 가기" href="/home" />
-                )}
-                <div className="loader" ref={observerRef}>
-                    {hasNextPage ? <Loading /> : null}
-                </div>
-            </ListBox>
-        </div>
+            <div css={Layout}>
+                <form>
+                    <Input
+                        inputStyle={'search'}
+                        placeholder={'댓글 내용 검색'}
+                        isResetBtn={!!search}
+                        handleReset={() => reset()}
+                        maxLength={8}
+                        {...register('search')}
+                    />
+                </form>
+                <ListBox>
+                    {commentList.length ? (
+                        commentList.map((item) => <ListItem key={item.id} id={item.id} content={item.content} createAt={item.createAt} />)
+                    ) : (
+                        <Empty title="작성한 댓글이 없습니다" subTitle="첫 댓글을 남겨보러 갈까요?" buttonTitle="댓글 작성하러 가기" href="/home" />
+                    )}
+                    <div className="loader" ref={observerRef}>
+                        {hasNextPage ? <Loading /> : null}
+                    </div>
+                </ListBox>
+            </div>
+        </>
     );
 };
 
--- pages/mypage/writeList/index.tsx
+++ pages/mypage/writeList/index.tsx
@@ -3,9 +3,9 @@ import { useForm } from 'react-hook-form';
 import { Header, Input, Loading } from '@components/Commons';
 import { Empty, ListBox, ListItem } from '@components/MyPageCom';
 // import { useObserver } from '@/hooks/useObserver';
-import { useGetPostsMe } from '@apis/posts';
 import EditSvg from '@assets/icons/edit.svg';
 import { Layout } from '@styles/pages/mypageStyled';
+import { useGetPostsMe } from '@/apis/post';
 
 const WriteList = () => {
     const observerRef = useRef(null);
@@ -52,40 +52,42 @@ const WriteList = () => {
     }, [fetchNextPage, hasNextPage, handleObserver]);
 
     return (
-        <div css={Layout}>
+        <>
             <Header title={'내가 작성한 글'} rightElement={<EditSvg />} />
-            <form>
-                <Input
-                    inputStyle={'search'}
-                    placeholder={'글 제목, 글 본문 내용 등 검색'}
-                    isResetBtn={!!search}
-                    handleReset={() => reset()}
-                    maxLength={8}
-                    {...register('search')}
-                />
-            </form>
-            <ListBox>
-                {writeList.length ? (
-                    writeList.map((item) => <ListItem key={item.id} id={item.id} content={item.content} createAt={item.createAt} />)
-                ) : (
-                    <>
-                        {search && search.length ? (
-                            <Empty
-                                title="검색 결과가 없습니다"
-                                subTitle="정확한 검색어를 입력해주세요"
-                                buttonTitle="글 작성하러 가기"
-                                href="/mypage/writeList"
-                            />
-                        ) : (
-                            <Empty title="작성한 글이 없습니다" subTitle="첫 글을 남겨보러 갈까요?" buttonTitle="목록으로" href="/home" />
-                        )}
-                    </>
-                )}
-                <div className="loader" ref={observerRef}>
-                    {hasNextPage ? <Loading /> : null}
-                </div>
-            </ListBox>
-        </div>
+            <div css={Layout}>
+                <form>
+                    <Input
+                        inputStyle={'search'}
+                        placeholder={'글 제목, 글 본문 내용 등 검색'}
+                        isResetBtn={!!search}
+                        handleReset={() => reset()}
+                        maxLength={8}
+                        {...register('search')}
+                    />
+                </form>
+                <ListBox>
+                    {writeList.length ? (
+                        writeList.map((item) => <ListItem key={item.id} id={item.id} content={item.content} createAt={item.createAt} />)
+                    ) : (
+                        <>
+                            {search && search.length ? (
+                                <Empty
+                                    title="검색 결과가 없습니다"
+                                    subTitle="정확한 검색어를 입력해주세요"
+                                    buttonTitle="글 작성하러 가기"
+                                    href="/mypage/writeList"
+                                />
+                            ) : (
+                                <Empty title="작성한 글이 없습니다" subTitle="첫 글을 남겨보러 갈까요?" buttonTitle="목록으로" href="/home" />
+                            )}
+                        </>
+                    )}
+                    <div className="loader" ref={observerRef}>
+                        {hasNextPage ? <Loading /> : null}
+                    </div>
+                </ListBox>
+            </div>
+        </>
     );
 };
 
