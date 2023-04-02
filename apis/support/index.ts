cfda9596fe515cbec765fc6e21c581b4be2c154f
leehyunju <hyunmail521@gmail.com>
2023-04-03 02:57:44 +0900

    fix : support 센터 api 정리 / 타입 정리

---
+++ apis/support/index.ts
@@ -0,0 +1,40 @@
+import Axios from '@utils/axios';
+import { IResponseBase } from '@/types/global';
+import { useQuery } from '@tanstack/react-query';
+import { ISupportModel } from '@/types/support';
+
+interface ISupportProps {
+    type: number;
+    content: string;
+}
+
+const getSupportCategory = async () => {
+    const res = await Axios.get<IResponseBase<ISupportModel[]>>('/support/type');
+
+    return res.data.data;
+};
+
+/**
+ * 서포트센터 게시글 유형
+ * @returns
+ */
+export const useGetSupportCategory = () => {
+    const { data } = useQuery(['getSupportCategory'], async () => {
+        const data = await getSupportCategory();
+
+        return data;
+    });
+
+    return data;
+};
+
+/**
+ * 서포트 센터 글 작성
+ * @param form
+ * @returns
+ */
+export const postSupport = async (form: ISupportProps) => {
+    const res = await Axios.post<IResponseBase<any>>('/support', { ...form });
+
+    return res.data;
+};
