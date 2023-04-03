c8cf62840d90b801ee31f4f329bf3ff24d8d95c3
leehyunju <hyunmail521@gmail.com>
2023-04-04 05:50:14 +0900

    create : 공지사항 리스트 페이지

---
+++ pages/mypage/notice/index.tsx
@@ -0,0 +1,64 @@
+import React, { useCallback, useEffect, useRef } from 'react';
+import { useForm } from 'react-hook-form';
+import { Header, Input, Loading } from '@components/Commons';
+import { Empty, ListBox, ListItem } from '@components/MyPageCom';
+
+import EditSvg from '@assets/icons/edit.svg';
+import { Layout } from '@styles/pages/mypageStyled';
+import { useGetNotice } from '@/apis/notice';
+
+const notice = () => {
+    const observerRef = useRef(null);
+
+    const { register, watch, reset } = useForm<{ search: string }>();
+    const { search } = watch();
+
+    const { contents: noticeList, hasNextPage, fetchNextPage } = useGetNotice(search);
+
+    const handleObserver = useCallback(
+        (entries: IntersectionObserverEntry[]) => {
+            const [target] = entries;
+            if (target.isIntersecting) {
+                fetchNextPage();
+            }
+        },
+        [fetchNextPage, hasNextPage],
+    );
+
+    useEffect(() => {
+        const element = observerRef.current;
+        const option = { threshold: 0 };
+
+        const observer = new IntersectionObserver(handleObserver, option);
+        if (element) {
+            observer.observe(element);
+            return () => observer.unobserve(element);
+        }
+    }, [fetchNextPage, hasNextPage, handleObserver]);
+
+    return (
+        <>
+            <Header title={'공지사항'} />
+            <div css={Layout}>
+                <form>
+                    <Input
+                        inputStyle={'search'}
+                        placeholder={'공지사항 검색'}
+                        isResetBtn={!!search}
+                        handleReset={() => reset()}
+                        maxLength={8}
+                        {...register('search')}
+                    />
+                </form>
+                <ListBox>
+                    {noticeList.length && noticeList.map((item) => <ListItem key={item.id} id={item.id} content={item.title} createAt={item.createAt} />)}
+                    <div className="loader" ref={observerRef}>
+                        {hasNextPage ? <Loading /> : null}
+                    </div>
+                </ListBox>
+            </div>
+        </>
+    );
+};
+
+export default notice;
