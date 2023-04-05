0b250af70178d162eab804b1d14f95271fb138fc
leehyunju <hyunmail521@gmail.com>
2023-04-06 07:44:51 +0900

    feat : 전체 게시판 작업완

---
+++ styles/pages/boardListStyled.ts
@@ -0,0 +1,18 @@
+import { css } from '@emotion/react';
+import color from '../color';
+
+export const BoardListStyle = () => css`
+    background-color: ${color.WHITE};
+    padding-bottom: 80px;
+    .boardListStyle--line {
+        border-bottom: 1px solid ${color.GRAY_BRIGHT_3};
+        padding-bottom: 44px;
+        li {
+            padding: 44px 0 0 43px;
+            color: ${color.GRAY_STRONG_5};
+            &.title {
+                font-weight: 700;
+            }
+        }
+    }
+`;
