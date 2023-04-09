699da3acdb96e85067abbe11723edfff2b89a139
leehyunju <hyunmail521@gmail.com>
2023-04-10 05:29:09 +0900

    [#34] feat : Scroll to Top 버튼 작업

--- components/Layout/index.tsx
+++ components/Layout/index.tsx
@@ -1,10 +1,49 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
 import { css } from '@emotion/react';
 import color from '@/styles/color';
+import TopButton from '@assets/icons/common/scrollToTop.svg';
 
 const Layout: React.FC<any> = (props) => {
     const { children } = props;
-    return <div css={StyledLayout}>{children}</div>;
+    const [showButton, setShowButton] = useState<boolean>(false);
+
+    // 👉 클릭 시 맨 위로 올라가도록
+    const handleScroll = () => {
+        if (!window.scrollY) return;
+
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth',
+        });
+    };
+
+    useEffect(() => {
+        const handleShowButton = () => {
+            if (window.scrollY > window.innerHeight) {
+                setShowButton(true);
+            } else {
+                setShowButton(false);
+            }
+        };
+
+        window.addEventListener('scroll', handleShowButton);
+        return () => {
+            window.removeEventListener('scroll', handleShowButton);
+        };
+    }, []);
+    return (
+        <div css={StyledLayout}>
+            {children}
+
+            {showButton && (
+                <div className="topBtn_wrap">
+                    <button className="topBtn" onClick={handleScroll}>
+                        <TopButton />
+                    </button>
+                </div>
+            )}
+        </div>
+    );
 };
 
 export default Layout;
@@ -17,4 +56,10 @@ const StyledLayout = css`
     width: 100%;
     height: 100vh;
     margin: 0 auto;
+    .topBtn_wrap {
+        position: sticky;
+        bottom: 106px;
+        float: right;
+        margin-right: 30px;
+    }
 `;
