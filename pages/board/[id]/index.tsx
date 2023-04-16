0450d701c0085722edec531c3749e44e13a0a991
khunjeong <51750678+khunjeong@users.noreply.github.com>
2023-04-16 20:22:43 +0900

    Refactor: board 수정

--- pages/board/[id]/index.tsx
+++ pages/board/[id]/index.tsx
@@ -1,5 +1,5 @@
-import { useState } from 'react';
 import { GetServerSideProps } from 'next';
+import { useRouter } from 'next/router';
 import Link from 'next/link';
 import FeedItem from '@/components/Home/FeedItem';
 import { Input, BottomMenu } from '@components/Commons';
@@ -8,55 +8,50 @@ import { useInfiniteQuery } from '@tanstack/react-query';
 import InfiniteScroll from 'react-infinite-scroller';
 import { HomeMenu, searchWrap } from '@styles/pages/homeStyled';
 import { getFeedPost } from '@/apis/post';
-import Search from '@/components/Home/Search';
 
 interface IBoardProps {
     id: number;
 }
 
 const board = ({ id }: IBoardProps) => {
-    const [search, setSearch] = useState<boolean>(false);
-    // 데이터 패칭
-    const { data, fetchNextPage, hasNextPage } = useInfiniteQuery(['page'], ({ pageParam = 0 }) => getFeedPost({ page: pageParam, categoryId: Number(id) }), {
-        getNextPageParam: (lastPage, allPosts) => {
-            return lastPage.page !== allPosts[0].totalPage ? lastPage.page + 1 : undefined;
-        },
-    });
+    const router = useRouter();
 
-    const searchClose = () => {
-        setSearch(false);
-    };
+    // 데이터 패칭
+    const { data, fetchNextPage, hasNextPage, isLoading } = useInfiniteQuery(
+        ['page'],
+        ({ pageParam = 0 }) => getFeedPost({ page: pageParam, categoryId: Number(id) }),
+        {
+            getNextPageParam: (lastPage, allPosts) => {
+                return lastPage.page !== allPosts[0].totalPage ? lastPage.page + 1 : undefined;
+            },
+        },
+    );
+
     return (
         <main>
-            {search ? (
-                <Search searchClose={searchClose} />
-            ) : (
-                <>
-                    {/* 헤더 */}
-                    <div css={HomeMenu}>
-                        <h1>MZTI</h1>
+            {/* 헤더 */}
+            <div css={HomeMenu}>
+                <h1>MZTI</h1>
 
-                        <div className="right">
-                            {/* TODO : 2차 오픈때 개발 예정 */}
-                            {/* <Link href="/alarm" className="alarm">
+                <div className="right">
+                    {/* TODO : 2차 오픈때 개발 예정 */}
+                    {/* <Link href="/alarm" className="alarm">
                         <AlarmIcon />
                     </Link> */}
-                            <Link href="/mypage">
-                                <MyPageIcon />
-                            </Link>
-                        </div>
-                    </div>
+                    <Link href="/mypage">
+                        <MyPageIcon />
+                    </Link>
+                </div>
+            </div>
 
-                    <div css={searchWrap}>
-                        <Input inputStyle={'search'} placeholder={'관심있는 MBTI, 키워드, 이슈 검색'} onFocus={() => setSearch(true)} />
-                    </div>
-                    {/* 피드 게시물 */}
-                    {data && (
-                        <InfiniteScroll hasMore={hasNextPage} loadMore={() => fetchNextPage()}>
-                            <FeedItem data={data} />
-                        </InfiniteScroll>
-                    )}
-                </>
+            <div css={searchWrap}>
+                <Input inputStyle={'search'} placeholder={'관심있는 MBTI, 키워드, 이슈 검색'} onClick={() => router.push(`/search/${id}`)} />
+            </div>
+            {/* 피드 게시물 */}
+            {data && (
+                <InfiniteScroll hasMore={hasNextPage} loadMore={() => fetchNextPage()}>
+                    <FeedItem data={data} isLoading={isLoading} />
+                </InfiniteScroll>
             )}
             {/* 메뉴 */}
             <BottomMenu />
