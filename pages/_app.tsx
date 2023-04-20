9b50c93b323c1acde1b65c266dda098275f97a4b
정재홍 <51750678+khunjeong@users.noreply.github.com>
2023-04-21 00:02:12 +0900

    Android, iOS 설치 Popup 작업내용 PR 올립니다. (#42)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,19 +1,39 @@
 import { useEffect, useMemo, useState } from 'react';
 import App, { AppContext, AppProps } from 'next/app';
+import dynamic from 'next/dynamic';
 import Head from 'next/head';
 import nextCookies from 'next-cookies';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import toast, { Toaster } from 'react-hot-toast';
+import { Toaster } from 'react-hot-toast';
 import { RecoilRoot, MutableSnapshot } from 'recoil';
 import { myPageInfo } from '@/recoil/atom/user';
 import { Global } from '@emotion/react';
 import Layout from '@components/Layout';
+import { Modal } from '@components/Commons';
 import globalReset from '@/styles/customReset';
 import axios from '@/utils/axios';
 import { removeTokenAll, setCookie } from '@utils/auth';
 import { getMeUserInfo } from '@apis/user';
 import { IUserModel } from '@/types/user';
+import usePWA from '@hooks/usePWA';
+
+type PWAPromptProps = Partial<{
+    copyAddHomeButtonLabel: string;
+    copyBody: string;
+    copyClosePrompt: string;
+    copyShareButtonLabel: string;
+    copyTitle: string;
+    debug: boolean;
+    delay: number;
+    permanentlyHideOnDismiss: boolean;
+    promptOnVisit: number;
+    timesToShow: number;
+}>;
+
+const PWAPrompt = dynamic<PWAPromptProps>(() => import('react-ios-pwa-prompt'), {
+    ssr: false,
+});
 
 interface IMyCustomApp extends AppProps {
     userInfo?: IUserModel;
@@ -22,6 +42,8 @@ interface IMyCustomApp extends AppProps {
 function MyCustomApp({ Component, pageProps, userInfo }: IMyCustomApp) {
     // 이렇게 해야 서로 다른 사용자와 요청 사이에 데이터가 공유되지 않는다.
     const [queryClient] = useState(() => new QueryClient());
+    const [isInstallPopup, setIsInstallPopup] = useState<boolean>(false);
+    const [notVisiblePopup, setNotVisiblePopup] = useState<boolean>(false);
 
     // user정보 초기값 설정
     const recoilInitializer = useMemo(
@@ -33,6 +55,28 @@ function MyCustomApp({ Component, pageProps, userInfo }: IMyCustomApp) {
             },
         [userInfo],
     );
+
+    const { isInstalled, canInstall, showInstallPrompt } = usePWA();
+
+    const onClickInstallPWA = () => {
+        showInstallPrompt();
+    };
+
+    const onCloseModal = () => {
+        setIsInstallPopup(false);
+        localStorage.setItem('notVisiblePopup', '');
+    };
+
+    useEffect(() => {
+        setNotVisiblePopup(localStorage.getItem('notVisiblePopup') !== null);
+    }, []);
+
+    useEffect(() => {
+        console.log({ isInstalled, canInstall, showInstallPrompt });
+        if (!notVisiblePopup && !isInstalled && canInstall) {
+            setIsInstallPopup(true);
+        }
+    }, [canInstall, isInstalled, notVisiblePopup]);
 
     useEffect(() => {
         if (userInfo && userInfo.id) {
@@ -60,6 +104,29 @@ function MyCustomApp({ Component, pageProps, userInfo }: IMyCustomApp) {
                     </Layout>
                 </RecoilRoot>
                 <ReactQueryDevtools />
+                {isInstallPopup && (
+                    <Modal
+                        title={'MZTI를 설치하고 싶은가요?'}
+                        isModalVisible={isInstallPopup}
+                        closable={false}
+                        footer={
+                            <>
+                                <button onClick={onCloseModal}>취소</button>
+                                <button onClick={onClickInstallPWA}>설치하기</button>
+                            </>
+                        }
+                    >
+                        <p>아래 계정으로 다시 로그인할 수 있습니다.</p>
+                    </Modal>
+                )}
+                <PWAPrompt
+                    copyAddHomeButtonLabel={'2) 「홈 화면에 추가」를 탭합니다.' || undefined}
+                    copyBody={'이 웹사이트에는 앱 기능이 있습니다.홈 화면에 추가하여 풀스크린 및 오프라인으로 사용할 수 있습니다.' || undefined}
+                    copyClosePrompt={'취소' || undefined}
+                    copyShareButtonLabel={'1) (사각에서 화살표가 튀어나온 마크)를 탭합니다.'}
+                    copyTitle={'홈 화면에 추가' || undefined}
+                    debug={process.env.NODE_ENV === 'development' && false}
+                />
             </QueryClientProvider>
         </>
     );
