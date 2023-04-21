25af7c01b2e358b08bb873d5c3440ef88efe7cc9
khunjeong <51750678+khunjeong@users.noreply.github.com>
2023-04-21 16:54:08 +0900

    Refactor: 댓글 수정 사진 업로드 추가

--- components/Commons/CommentModifyInput/index.tsx
+++ components/Commons/CommentModifyInput/index.tsx
@@ -1,11 +1,14 @@
+import { ChangeEvent, useEffect, useState, useRef } from 'react';
+import Image from 'next/image';
 import { CommentInputStyle } from './styled';
 import PhotoIcon from '@assets/icons/comment/photo.svg';
+import CloseButtonIcon from '@assets/icons/comment/close.svg';
 import { useSetRecoilState } from 'recoil';
 import { commentModify, commentText } from '@/recoil/atom/user';
 
 interface PostIdProps {
     onSuccess: () => void; // 성공시 다시 refresh 할 데이터
-    PutComment?: () => void; // 댓글 추가 api
+    PutComment?: (imageFile?: File) => void; // 댓글 추가 api
     handleContact?: (e: any) => void; // 댓글 내용 바뀔 때
     commentId?: number;
     comment: string; // value 값
@@ -16,15 +19,56 @@ const CommentModifyInput = ({ comment, PutComment, handleContact }: PostIdProps)
     const setCommentModifyState = useSetRecoilState(commentModify);
     const setCommentValue = useSetRecoilState(commentText);
 
+    const [previewFileSrc, setPreviewFileSrc] = useState<string>();
+    const [imageFile, setImageFile] = useState<File>();
+
+    const imgInputRef = useRef<HTMLInputElement | null>(null);
+
+    // 이미지 로컬 링크 해제
+    const revokeImageLink = () => {
+        if (previewFileSrc) {
+            URL.revokeObjectURL(previewFileSrc);
+            setPreviewFileSrc(undefined);
+        }
+    };
+
+    const handleUpdateProfileImg = ({ target: { files } }: ChangeEvent<HTMLInputElement>) => {
+        if (files) {
+            const file = files[0];
+            // 이미지 파일 저장
+            setImageFile(file);
+
+            // 이미지 로컬 링크 생성
+            setPreviewFileSrc(URL.createObjectURL(new Blob([file])));
+        }
+    };
+
+    useEffect(() => {
+        return () => {
+            revokeImageLink();
+        };
+    }, []);
+
     const CancelComment = () => {
         setCommentModifyState(false);
         setCommentValue('');
     };
     return (
         <div css={CommentInputStyle}>
-            <button>
+            {previewFileSrc && (
+                <div className="image_box">
+                    <button onClick={revokeImageLink}>
+                        <CloseButtonIcon />
+                    </button>
+                    <Image src={previewFileSrc} alt={'대댓글이미지'} width={100} height={100} />
+                </div>
+            )}
+
+            <button onClick={() => imgInputRef.current && imgInputRef.current.click()}>
                 <PhotoIcon />
             </button>
+
+            <input ref={imgInputRef} type="file" name="file" accept="image/*" style={{ display: 'none' }} onChange={handleUpdateProfileImg} />
 
             <input type="text" value={comment} placeholder="댓글을 입력해주세요" onChange={handleContact} />
 
@@ -32,7 +76,14 @@ const CommentModifyInput = ({ comment, PutComment, handleContact }: PostIdProps)
                 <button type="submit" onClick={CancelComment}>
                     취소
                 </button>
-                <button type="submit" onClick={PutComment} disabled={!comment}>
+                <button
+                    type="submit"
+                    onClick={() => {
+                        PutComment && PutComment(imageFile);
+                        revokeImageLink();
+                    }}
+                    disabled={!comment}
+                >
                     변경
                 </button>
             </div>
--- components/Commons/CommentModifyInput/styled.ts
+++ components/Commons/CommentModifyInput/styled.ts
@@ -2,7 +2,7 @@ import color from '@/styles/color';
 import { css } from '@emotion/react';
 
 export const CommentInputStyle = css`
-    position: fixed;
+    position: sticky;
     background: ${color.GRAY_LIGHT_5};
     padding: 15px 20px;
     height: 80px;
@@ -12,10 +12,37 @@ export const CommentInputStyle = css`
     font-weight: 400;
     font-size: 1.4rem;
     bottom: 0;
+    left: 0;
     display: flex;
     align-items: center;
     justify-content: space-between;
     input {
         width: 80%;
+    }
+    .image_box {
+        display: flex;
+        flex-direction: column;
+        align-items: flex-end;
+        justify-content: center;
+        position: absolute;
+        top: -150px;
+        left: 0;
+        width: 100%;
+        height: 150px;
+        padding: 8px;
+        z-index: 0;
+        background: rgba(0, 0, 0, 0.8);
+        button {
+            position: absolute;
+            top: 10px;
+            svg {
+                path {
+                    fill: ${color.WHITE};
+                }
+            }
+        }
+        img {
+            margin-right: 30px;
+        }
     }
 `;
