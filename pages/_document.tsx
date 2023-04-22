f05cbb96f45aff11eca33f7104c4b21a56543796
khunjeong <51750678+khunjeong@users.noreply.github.com>
2023-04-22 13:42:04 +0900

    Refactor: iOS 설치시 App Icon 변경

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,16 +5,10 @@ export default function Document() {
         <Html lang="en">
             <Head>
                 <link rel="manifest" href="/manifest.json" />
-                <link rel="apple-touch-icon" sizes="57x57" href="/icons/apple-icon-57x57.png" />
-                <link rel="apple-touch-icon" sizes="60x60" href="/icons/apple-icon-60x60.png" />
-                <link rel="apple-touch-icon" sizes="72x72" href="/icons/apple-icon-72x72.png" />
-                <link rel="apple-touch-icon" sizes="76x76" href="/icons/apple-icon-76x76.png" />
-                <link rel="apple-touch-icon" sizes="114x114" href="/icons/apple-icon-114x114.png" />
-                <link rel="apple-touch-icon" sizes="120x120" href="/icons/apple-icon-120x120.png" />
-                <link rel="apple-touch-icon" sizes="144x144" href="/icons/apple-icon-144x144.png" />
-                <link rel="apple-touch-icon" sizes="152x152" href="/icons/apple-icon-152x152.png" />
-                <link rel="apple-touch-icon" sizes="180x180" href="/icons/apple-icon-180x180.png" />
-                <link rel="icon" type="image/png" sizes="192x192" href="/icons/android-icon-192x192.png" />
+                <link rel="apple-touch-icon" href="/icons/icon-152x152.png" />
+                <link rel="apple-touch-icon" sizes="144x144" href="/icons/icon-144x144.png" />
+                <link rel="apple-touch-icon" sizes="152x152" href="/icons/icon-152x152.png" />
+                <link rel="apple-touch-icon" sizes="192x192" href="/icons/icon-192x192.png" />
                 <link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon-32x32.png" />
                 <link rel="icon" type="image/png" sizes="96x96" href="/icons/favicon-96x96.png" />
                 <link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon-16x16.png" />
@@ -25,6 +19,7 @@ export default function Document() {
                 <meta property="og:image" content={`https://velog.velcdn.com/images/leemember/post/7767e642-e4ec-403e-af16-6ecda22a6f36/image.png`} />
                 <meta property="og:description" content="🅼🆉🆃🅸 | MBTI에 과몰입할 MZ세대들 모여라! | MBTI 뇌절 콘텐츠, MBTI 궁합, MBTI 짤 등" />
                 <meta name="twitter:card" content="summary_large_image" />
+                <meta name="apple-mobile-web-app-capable" content="yes" />
             </Head>
             <body>
                 <Main />
