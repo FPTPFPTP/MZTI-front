9f62d0f9c6b6b712d454704df30e00900c4424e1
khunjeong <51750678+khunjeong@users.noreply.github.com>
2023-04-24 17:46:05 +0900

    Refactor: 설치 유도 팝업창 desktop 제외

--- utils/window.ts
+++ utils/window.ts
@@ -1,3 +1,16 @@
 export const isWindow = () => {
     return typeof window !== 'undefined';
 };
+
+/**
+ * 디바이스 체크
+ * @return {string / boolean}
+ */
+export const device = () => {
+    if (/Android/i.test(navigator.userAgent)) {
+        return 'Android';
+    } else if (/iPhone|iPad|iPod/i.test(navigator.userAgent)) {
+        return 'iPhone';
+    }
+    return false;
+};
