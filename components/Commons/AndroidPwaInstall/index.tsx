d843603aa3ba61995ea45e6221046c378acaf5ec
khunjeong <51750678+khunjeong@users.noreply.github.com>
2023-04-24 22:10:55 +0900

    Refactor: 닉네임 중복 체크 로직 적용

---
+++ components/Commons/AndroidPwaInstall/index.tsx
@@ -0,0 +1,12 @@
+const AndroidPwaInstall = () => {
+    return (
+        <div>
+            <h1>홈 화면에 추가</h1>
+            <div></div>
+            <p>이 웹사이트에는 앱 기능이 있습니다.홈 화면에 추가하여 풀스크린 및 오프라인으로 사용할 수 있습니다.</p>
+            <button></button>
+        </div>
+    );
+};
+
+export default AndroidPwaInstall;
