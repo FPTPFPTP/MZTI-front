3a958593259de9131f64157f96aa92b72235df11
leehyunju <hyunmail521@gmail.com>
2023-04-28 17:53:13 +0900

    feat : 인기게시판, 전체게시판, Drawer 드롭 팝업 작업중

--- components/Home/ListTab/index.tsx
+++ components/Home/ListTab/index.tsx
@@ -2,30 +2,33 @@ import { useState } from 'react';
 import { ListTabStyle } from '../styled';
 import classnames from 'classnames';
 
+interface IListTabProps {
+    handleCategoryId: (id?: number) => void;
+    categoryId?: number;
+}
 const menus = [
     {
+        categoryId: 22,
         name: '인기 게시판',
-        path: '/board/22',
         isActive: false,
     },
     {
+        categoryId: undefined,
         name: '전체 게시판',
-        path: '/',
         isActive: false,
     },
 ];
 
-const ListTab = () => {
-    const [countIndex, setCountIndex] = useState<number>(0);
-
+const ListTab = ({ handleCategoryId, categoryId }: IListTabProps) => {
     const handleOnClick = (idx: number) => {
-        setCountIndex(idx);
+        handleCategoryId(idx);
     };
+
     return (
         <section css={ListTabStyle}>
             {menus.map((item, idx: number) => {
                 return (
-                    <button onClick={() => handleOnClick(idx)} className={classnames(countIndex === idx && 'active')} key={idx}>
+                    <button onClick={() => handleOnClick(idx)} className={classnames(categoryId === idx && 'active')} key={idx}>
                         {item.name}
                     </button>
                 );
--- pages/home/index.tsx
+++ pages/home/index.tsx
@@ -1,4 +1,6 @@
 import { useRouter } from 'next/router';
+import { Empty } from '@/components/MyPageCom';
+import { useState } from 'react';
 import FeedItem from '@/components/Home/FeedItem';
 import HotKeyword from '@/components/Home/HotKeyword';
 import { Input, BottomMenu } from '@components/Commons';
@@ -7,17 +9,23 @@ import InfiniteScroll from 'react-infinite-scroller';
 import { searchWrap } from '@styles/pages/homeStyled';
 import ListTab from '@/components/Home/ListTab';
 import { getFeedPost } from '@/apis/post';
+import EmptyWrite from '@assets/icons/common/empty_write.svg';
 import FeedHeader from '@/components/Commons/FeedHeader';
 
 const home = () => {
     const router = useRouter();
+    const [countIndex, setCountIndex] = useState<number | undefined>(22);
 
     // 데이터 패칭
-    const { data, fetchNextPage, hasNextPage, isLoading } = useInfiniteQuery(['page'], ({ pageParam = 0 }) => getFeedPost({ page: pageParam }), {
-        getNextPageParam: (lastPage, allPosts) => {
-            return lastPage.page !== allPosts[0].totalPage ? lastPage.page + 1 : undefined;
+    const { data, fetchNextPage, hasNextPage, isLoading } = useInfiniteQuery(
+        ['page', countIndex],
+        ({ pageParam = 0 }) => getFeedPost({ page: pageParam, categoryId: countIndex }),
+        {
+            getNextPageParam: (lastPage, allPosts) => {
+                return lastPage.page !== allPosts[0].totalPage ? lastPage.page + 1 : undefined;
+            },
         },
-    });
+    );
 
     return (
         <main className="homeLayout">
@@ -29,19 +37,20 @@ const home = () => {
                     <Input inputStyle={'search'} placeholder={'관심있는 MBTI, 키워드, 이슈 검색'} onClick={() => router.push('/search')} />
                 </div>
 
-                {data?.pages[0].list.length !== 0 && (
-                    <>
-                        {/* 인기 게시판 & 전체 게시판 */}
-                        <ListTab />
-                        {/* 핫토픽 키워드 */}
-                        <HotKeyword title="🔥실시간 HOT 키워드" more={true} />
-                    </>
-                )}
+                <>
+                    {/* 인기 게시판 & 전체 게시판 */}
+                    <ListTab categoryId={countIndex} handleCategoryId={(id) => setCountIndex(id)} />
+                    {/* 핫토픽 키워드 */}
+                    <HotKeyword title="🔥실시간 HOT 키워드" more={true} />
+                </>
+
                 {/* 피드 게시물 */}
-                {data && (
+                {data && data.pages.length && data.pages[0].list.length !== 0 ? (
                     <InfiniteScroll hasMore={hasNextPage} loadMore={() => fetchNextPage()}>
                         <FeedItem data={data} isLoading={isLoading} />
                     </InfiniteScroll>
+                ) : (
+                    <Empty icon={<EmptyWrite />} title="작성된 글이 없습니다." subTitle={`게시글을 작성해주세요`} />
                 )}
             </>
             {/* 메뉴 */}
