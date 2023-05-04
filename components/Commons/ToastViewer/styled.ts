a1b43e2c93db1efb6e5055737d226ae86148a49a
khunjeong <51750678+khunjeong@users.noreply.github.com>
2023-05-05 00:28:07 +0900

    Refactor: ToastViewer css 수정

--- components/Commons/ToastViewer/styled.ts
+++ components/Commons/ToastViewer/styled.ts
@@ -2,10 +2,20 @@ import { css } from '@emotion/react';
 
 export const ToastViewerStyle = css`
     padding: 0;
+    margin-top: 50px;
     iframe {
         width: 100%;
         max-width: 500px;
         min-height: 300px;
         max-height: 500px;
+    }
+    .toastui-editor-contents p {
+        font-family: Pretendard;
+        font-size: 15px;
+        font-weight: 500;
+        line-height: 23px;
+        letter-spacing: -0.40799999237060547px;
+        text-align: left;
+        margin-top: 0px;
     }
 `;
