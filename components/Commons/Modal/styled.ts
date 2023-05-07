f5605705ec41ee8bb6b095ba7fe4bd82de38fd4a
leehyunju <hyunmail521@gmail.com>
2023-05-07 18:39:08 +0900

    feat : 모달 재정비 작업 / 알림 클릭 시 모달 오픈

--- components/Commons/Modal/styled.ts
+++ components/Commons/Modal/styled.ts
@@ -13,25 +13,39 @@ export const ModalStyle = css`
         border-radius: 20px !important;
     }
 
+    .button {
+        margin-top: 17px;
+        width: 100%;
+        padding: 11px 0;
+        border-top: 0.5px solid rgba(0, 0, 0, 0.3);
+    }
     .ant-modal {
         width: 100% !important;
         padding: 0 45px !important;
         max-width: 100% !important;
         .ant-modal-title {
-            font-weight: 400;
-            font-size: 24px;
-            line-height: 32px;
+            font-weight: 700;
+            font-size: 17px;
+            line-height: 22px;
+            text-align: center;
+            letter-spacing: -0.408px;
+            color: #1b1e26;
         }
         .ant-modal-body {
-            font-weight: 400;
-            font-size: 14px;
-            line-height: 20px;
+            font-weight: 500;
+            font-size: 13px;
+            line-height: 16px;
+            text-align: center;
+            letter-spacing: -0.078px;
+            color: #545456;
         }
         .ant-modal-footer {
             button {
-                font-weight: 500;
-                font-size: 14px;
-                line-height: 20px;
+                font-weight: 400;
+                font-size: 17px;
+                line-height: 22px;
+                text-align: center;
+                letter-spacing: -0.408px;
             }
         }
         &-header {
@@ -55,10 +69,6 @@ export const ModalStyle = css`
             padding: 0px;
             color: ${colors.GRAY_STRONG_5};
             font-size: 1rem;
-        }
-
-        &-body {
-            padding: 0px 20px;
         }
 
         &-footer {
