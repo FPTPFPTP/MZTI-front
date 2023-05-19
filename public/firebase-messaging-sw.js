e5b1d851752f19633fbf21c737d56f19c546c6b9
khunjeong <51750678+khunjeong@users.noreply.github.com>
2023-05-19 14:51:03 +0900

    Chore: gitIgnore 수정

---
+++ public/firebase-messaging-sw.js
@@ -0,0 +1,27 @@
+importScripts('https://www.gstatic.com/firebasejs/9.22.0/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/9.22.0/firebase-messaging-compat.js');
+
+const config = {
+    apiKey: 'AIzaSyCvjjhyHXfNJazxCiskCicW6mc2YEAauQQ',
+    authDomain: 'mzti-a6373.firebaseapp.com',
+    projectId: 'mzti-a6373',
+    storageBucket: 'mzti-a6373.appspot.com',
+    messagingSenderId: '324938800012',
+    appId: '1:324938800012:web:b5bce87dc608e2233014a9',
+    measurementId: 'G-TS7NV20XG1',
+};
+
+// Initialize Firebase
+firebase.initializeApp(config);
+
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage((payload) => {
+    console.log('[firebase-messaging-sw.js] Received background message ', payload);
+    const notificationTitle = payload.notification.title;
+    const notificationOptions = {
+        body: payload.notification.body,
+    };
+
+    return self.registration.showNotification(notificationTitle, notificationOptions);
+});
