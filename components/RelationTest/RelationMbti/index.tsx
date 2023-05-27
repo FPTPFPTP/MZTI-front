8c839ff1469a4813cbfeafe01f7a9e411e24438b
khunjeong <51750678+khunjeong@users.noreply.github.com>
2023-05-27 11:21:40 +0900

    Refactor: 상대 MBTI 입력 적용으로 파일 수정

--- components/RelationTest/RelationMbti/index.tsx
+++ components/RelationTest/RelationMbti/index.tsx
@@ -6,18 +6,19 @@ import { Input, Mbti } from '@components/Commons';
 import { IRelationTestModel } from '@/types/relationTest';
 import { ContainerStyle } from './styled';
 
-const MyMbti = () => {
+interface IRelationMbti {
+    step: number;
+}
+const RelationMbti = ({ step }: IRelationMbti) => {
     const [relationTestStateObj, setRelationTestStateObj] = useRecoilState(relationTestState);
 
-    const { register, handleSubmit, watch, reset, setValue } = useForm<IRelationTestModel>();
-    const { myName } = watch();
-
-    const onSubmit = (data: { myName?: string }) => {
-        setRelationTestStateObj({ ...relationTestStateObj, myName: data.myName || '' });
-    };
+    const { register, watch, reset, setValue } = useForm<IRelationTestModel>();
+    const { myName, partnerName } = watch();
 
     const onUpdateMbti = (mbti: string) => {
-        setRelationTestStateObj({ ...relationTestStateObj, myMbti: mbti });
+        step === 2
+            ? setRelationTestStateObj({ ...relationTestStateObj, myMbti: mbti })
+            : setRelationTestStateObj({ ...relationTestStateObj, partnerMbti: mbti });
     };
 
     // 페이지 접근시 이전에 작성한 값이 있으면 적용
@@ -25,26 +26,35 @@ const MyMbti = () => {
         if (relationTestStateObj.myName && relationTestStateObj.myName.length) {
             setValue('myName', relationTestStateObj.myName);
         }
+        if (relationTestStateObj.partnerName && relationTestStateObj.partnerName.length) {
+            setValue('partnerName', relationTestStateObj.partnerName);
+        }
     }, []);
 
     useEffect(() => {
         if (myName && myName.length) {
-            setRelationTestStateObj({ ...relationTestStateObj, myName: myName });
+            setRelationTestStateObj({ ...relationTestStateObj, myName });
         }
     }, [myName]);
+
+    useEffect(() => {
+        if (partnerName && partnerName.length) {
+            setRelationTestStateObj({ ...relationTestStateObj, partnerName });
+        }
+    }, [partnerName]);
 
     return (
         <div css={ContainerStyle}>
             <div className="name">
-                <p>내 이름</p>
-                <form onSubmit={handleSubmit(onSubmit)}>
+                <p>{step === 2 ? '내 이름' : '상대 이름'}</p>
+                <form>
                     <Input
                         inputStyle={'borderLess'}
                         placeholder={'실명이 아닌 별명도 괜찮아요'}
                         isResetBtn={!!relationTestStateObj.myName}
                         handleReset={() => reset()}
                         maxLength={8}
-                        {...register('myName', {
+                        {...register(step === 2 ? 'myName' : 'partnerName', {
                             required: true,
                         })}
                     />
@@ -52,11 +62,11 @@ const MyMbti = () => {
                 </form>
             </div>
             <div className="mbti">
-                <p>내 MBTI</p>
-                <Mbti mbti={relationTestStateObj.myMbti} onUpdateMbti={onUpdateMbti} />
+                <p>{step === 2 ? '내 MBTI' : '상대 MBTI'}</p>
+                <Mbti mbti={step === 2 ? relationTestStateObj.myMbti : relationTestStateObj.partnerMbti} onUpdateMbti={onUpdateMbti} />
             </div>
         </div>
     );
 };
 
-export default MyMbti;
+export default RelationMbti;
