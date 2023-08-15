35d7f550628d2f7ebfef52a073c7bc5e78e4cb3d
khunjeong <51750678+khunjeong@users.noreply.github.com>
2023-08-15 21:49:34 +0900

    Refactor: 야매테스트 페이지 SEO 적용

--- pages/simpleMbti/index.tsx
+++ pages/simpleMbti/index.tsx
@@ -5,7 +5,7 @@ import { simpleMbtiState } from '@/recoil/atom/simpleMbti';
 import MztiLogoIcon from '@assets/icons/simpleTest/mzti_logo_1.svg';
 import RightArrowIcon from '@assets/icons/simpleTest/right_arrow.svg';
 import ShareIcon from '@assets/icons/simpleTest/share.svg';
-import { Button } from '@/components/Commons';
+import { Button, HeadMeta } from '@/components/Commons';
 import { LinkCopy } from '@/utils/copy';
 import { SimpleTestStyle } from '@styles/pages/simpleTestStyled';
 import { device } from '@/utils/window';
@@ -23,33 +23,36 @@ const simpleMbti = () => {
     }, []);
 
     return (
-        <div css={SimpleTestStyle(isMobile)}>
-            <div className={'logo'}>
-                <MztiLogoIcon />
-            </div>
-            <div className={'intro'}>
-                10분 걸리는 MBTI 검사,
-                <br /> 10초만에 간단히 해보자!
-            </div>
-            <div className={'title'}>
-                <p className={'text'}>10초 끝!</p>
-                <br />
-                <p>
-                    간단 <span style={{ color: '#F56326' }}>MBTI</span> 테스트
-                </p>
-            </div>
+        <>
+            <HeadMeta title={'10초 MBTI 테스트'} description={'10분 걸리는 MBTI 검사, 10초만에 간단히 해보자!'} url={router.asPath} />
+            <main css={SimpleTestStyle(isMobile)}>
+                <div className={'logo'}>
+                    <MztiLogoIcon />
+                </div>
+                <div className={'intro'}>
+                    10분 걸리는 MBTI 검사,
+                    <br /> 10초만에 간단히 해보자!
+                </div>
+                <div className={'title'}>
+                    <p className={'text'}>10초 끝!</p>
+                    <br />
+                    <p>
+                        간단 <span style={{ color: '#F56326' }}>MBTI</span> 테스트
+                    </p>
+                </div>
 
-            <div className={'action'}>
-                <Button buttonStyle="base" onClick={() => router.push('/simpleMbti/test')}>
-                    바로 테스트하기
-                    <RightArrowIcon />
-                </Button>
-                <Button buttonStyle="base" onClick={LinkCopy} style={{ background: '#86888D' }}>
-                    테스트 공유하기
-                    <ShareIcon />
-                </Button>
-            </div>
-        </div>
+                <div className={'action'}>
+                    <Button buttonStyle="base" onClick={() => router.push('/simpleMbti/test')}>
+                        바로 테스트하기
+                        <RightArrowIcon />
+                    </Button>
+                    <Button buttonStyle="base" onClick={LinkCopy} style={{ background: '#86888D' }}>
+                        테스트 공유하기
+                        <ShareIcon />
+                    </Button>
+                </div>
+            </main>
+        </>
     );
 };
 
