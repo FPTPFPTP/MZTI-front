496f40b1410abaf1d7f198e37e3c2749747ea9c7
khunjeong <51750678+khunjeong@users.noreply.github.com>
2023-08-15 21:52:53 +0900

    Refactor: board 게시판 SEO 수정

--- pages/board/[id]/index.tsx
+++ pages/board/[id]/index.tsx
@@ -3,7 +3,7 @@ import { useRouter } from 'next/router';
 import { useRecoilValue } from 'recoil';
 import { prevScrollState } from '@/recoil/atom/scroll';
 import FeedItem from '@/components/Home/FeedItem';
-import { Header, Input, BottomMenu } from '@components/Commons';
+import { Header, Input, BottomMenu, HeadMeta } from '@components/Commons';
 import { useInfiniteQuery } from '@tanstack/react-query';
 import InfiniteScroll from 'react-infinite-scroller';
 import { getFeedPost, useGetMbtiNotice } from '@/apis/post';
@@ -55,55 +55,58 @@ const board = ({ id }: IBoardProps) => {
     }, [router.pathname]);
 
     return (
-        <main className="homeLayout">
-            {/* 헤더 */}
-            <Header isPrevBtn={true} title={`${categoryTitle} 게시판`} isBorderLine={false} />
+        <>
+            <HeadMeta title={`${categoryTitle} 게시판`} url={router.asPath} />
+            <main className="homeLayout">
+                {/* 헤더 */}
+                <Header isPrevBtn={true} title={`${categoryTitle} 게시판`} isBorderLine={false} />
 
-            <div css={FeedContentStyle}>
-                <div css={SearchWrapStyle} style={{ margin: '0px 0px 10px' }}>
-                    <Input inputStyle={'search'} placeholder={'관심있는 MBTI, 키워드, 이슈 검색'} onClick={() => router.push(`/search/${id}`)} />
+                <div css={FeedContentStyle}>
+                    <div css={SearchWrapStyle} style={{ margin: '0px 0px 10px' }}>
+                        <Input inputStyle={'search'} placeholder={'관심있는 MBTI, 키워드, 이슈 검색'} onClick={() => router.push(`/search/${id}`)} />
+                    </div>
+
+                    {noticeApi?.length !== 0 && (
+                        <ul className="mbtiNotice">
+                            {noticeApi?.map((item) => {
+                                return (
+                                    <li key={item.id}>
+                                        <Link href={`/boardDetail/${item.id}`}>
+                                            <span>
+                                                <SpeechIcon />
+                                            </span>
+                                            <p>{item.title}</p>
+                                        </Link>
+                                    </li>
+                                );
+                            })}
+                        </ul>
+                    )}
+
+                    {/* 피드 게시물 */}
+                    {isLoading ? (
+                        <>
+                            <FeedSkeleton />
+                            <FeedSkeleton />
+                            <FeedSkeleton />
+                        </>
+                    ) : (
+                        <>
+                            {data && data.pages.length && data.pages[0].list.length !== 0 ? (
+                                <InfiniteScroll hasMore={hasNextPage} loadMore={() => fetchNextPage()}>
+                                    <FeedItem data={data} />
+                                </InfiniteScroll>
+                            ) : (
+                                <Empty icon={<EmptyWrite />} title="작성된 글이 없습니다." subTitle={`게시글을 작성해주세요`} />
+                            )}
+                        </>
+                    )}
                 </div>
 
-                {noticeApi?.length !== 0 && (
-                    <ul className="mbtiNotice">
-                        {noticeApi?.map((item) => {
-                            return (
-                                <li key={item.id}>
-                                    <Link href={`/boardDetail/${item.id}`}>
-                                        <span>
-                                            <SpeechIcon />
-                                        </span>
-                                        <p>{item.title}</p>
-                                    </Link>
-                                </li>
-                            );
-                        })}
-                    </ul>
-                )}
-
-                {/* 피드 게시물 */}
-                {isLoading ? (
-                    <>
-                        <FeedSkeleton />
-                        <FeedSkeleton />
-                        <FeedSkeleton />
-                    </>
-                ) : (
-                    <>
-                        {data && data.pages.length && data.pages[0].list.length !== 0 ? (
-                            <InfiniteScroll hasMore={hasNextPage} loadMore={() => fetchNextPage()}>
-                                <FeedItem data={data} />
-                            </InfiniteScroll>
-                        ) : (
-                            <Empty icon={<EmptyWrite />} title="작성된 글이 없습니다." subTitle={`게시글을 작성해주세요`} />
-                        )}
-                    </>
-                )}
-            </div>
-
-            {/* 메뉴 */}
-            <BottomMenu />
-        </main>
+                {/* 메뉴 */}
+                <BottomMenu />
+            </main>
+        </>
     );
 };
 
