76788a249c49d2411380410f27dc3bbf96f0a6e3
khunjeong <51750678+khunjeong@users.noreply.github.com>
2023-10-02 16:48:00 +0900

    Refactor: manifest 조정

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@
 const withPWA = require('next-pwa')({
     dest: 'public',
     disable: process.env.NODE_ENV === 'development',
+    publicExcludes: ['!robots.txt', '!sitemap.xml.gz'],
 });
 
 module.exports = withPWA({
